5465d047cd48f09a9aa9dfadfd87d5073bb02903
Rohit Singhal <rohits.06oct@gmail.com>
2026-04-05 20:17:24 +0530

    API changes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Centralized frontend configuration
 const CONFIG = {
     // Update this URL if your Render service name changes
-    API_BASE_URL: 'https://test-backend.onrender.com/api/comments'
+    API_BASE_URL: 'https://test-backend-zq2u.onrender.com/api/comments'
 };
